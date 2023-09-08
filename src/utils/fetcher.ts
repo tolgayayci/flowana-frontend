a7f2f1751e5588f73c95c3b1d2b3624f5fb2c00b
Tolga Yaycı <40897846+tolgayayci@users.noreply.github.com>
2023-09-08 21:11:59 +0300

    vercel fix

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,12 +1,17 @@
 import axios from "axios";
 
-export const fetcher = (url: string) => 
-  axios
-    .get(
-      url.startsWith("http") ? url : process.env.NEXT_PUBLIC_API_URL + url,
-      {
-        headers: { Authorization: `Bearer ${process.env.NEXT_PUBLIC_AUTH_TOKEN}` },
-        validateStatus: (status) => status !== 204 // Ignore 204 status
-      }
-    )
-    .then((res) => (res.status !== 204 ? res.data : undefined));
+export const fetcher = (url: string) =>  {
+
+  console.log("URL: ", url)
+  console.log("process.env.NEXT_PUBLIC_API_URL: ", process.env.NEXT_PUBLIC_API_URL)
+
+  console.log("combined", process.env.NEXT_PUBLIC_API_URL + url)
+  return axios
+    .get(process.env.NEXT_PUBLIC_API_URL + url, {
+      headers: { Authorization: `Bearer ${process.env.NEXT_PUBLIC_AUTH_TOKEN}`,
+    },
+      validateStatus: (status) => status !== 204 // Ignore 204 status
+    })
+    .then((res) => (res.status !== 204 ? res.data : undefined))
+}
+  
