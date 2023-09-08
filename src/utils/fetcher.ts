72bad9e21eed6e558541a5a2029bda0e8e041d54
Tolga Yaycı <40897846+tolgayayci@users.noreply.github.com>
2023-09-08 20:57:38 +0300

    axios config updated

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,9 +1,12 @@
 import axios from "axios";
 
-export const fetcher = (url: string, protocol?: string) => 
+export const fetcher = (url: string) => 
   axios
-    .get(process.env.NEXT_PUBLIC_API_URL + url, {
-      headers: { Authorization: `Bearer ${process.env.NEXT_PUBLIC_AUTH_TOKEN}` },
-      validateStatus: (status) => status !== 204 // Ignore 204 status
-    })
+    .get(
+      url.startsWith("http") ? url : process.env.NEXT_PUBLIC_API_URL + url,
+      {
+        headers: { Authorization: `Bearer ${process.env.NEXT_PUBLIC_AUTH_TOKEN}` },
+        validateStatus: (status) => status !== 204 // Ignore 204 status
+      }
+    )
     .then((res) => (res.status !== 204 ? res.data : undefined));
